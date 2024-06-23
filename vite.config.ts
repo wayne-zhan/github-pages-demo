e56dbe003f0336a81ea949247ac68d4542fdec20
wayne-zhan <1464867716@qq.com>
2024-06-23 15:48:15 +0800

    feat: docs 目录

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
